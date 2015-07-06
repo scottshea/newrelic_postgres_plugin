a6f302630416fe976ea9b3e2dea9c0da8ab9e1d0
Scott Shea <scott.j.shea@gmail.com>
2015-07-06 11:12:38 -0700

    Update GUID and change double quotes to single quotes where useful

--- lib/newrelic_postgres_plugin/agent.rb
+++ lib/newrelic_postgres_plugin/agent.rb
@@ -12,7 +12,7 @@ module NewRelic::PostgresPlugin
 
   class Agent < NewRelic::Plugin::Agent::Base
 
-    agent_guid    'com.boundless.postgres'
+    agent_guid    'com.scottshea.postgres'
     agent_version NewRelic::PostgresPlugin::VERSION
     agent_config_options :host, :port, :user, :password, :dbname, :sslmode, :label
     agent_human_labels('Postgres') { "#{label || host}" }
@@ -66,42 +66,42 @@ module NewRelic::PostgresPlugin
 
     def report_metrics
       @connection.exec(backend_query) do |result|
-        report_metric "Backends/Active", 'connections', result[0]['backends_active']
-        report_metric "Backends/Idle",   'connections', result[0]['backends_idle']
+        report_metric 'Backends/Active', 'connections', result[0]['backends_active']
+        report_metric 'Backends/Idle',   'connections', result[0]['backends_idle']
       end
       @connection.exec(database_query) do |result|
         result.each do |row|
-          report_metric         "Database/Backends",                 '', row['numbackends'].to_i
-          report_derived_metric "Database/Transactions/Committed",   'transactions', row['xact_commit'].to_i
-          report_derived_metric "Database/Transactions/Rolled Back", 'transactions', row['xact_rollback'].to_i
+          report_metric         'Database/Backends',                 '', row['numbackends'].to_i
+          report_derived_metric 'Database/Transactions/Committed',   'transactions', row['xact_commit'].to_i
+          report_derived_metric 'Database/Transactions/Rolled Back', 'transactions', row['xact_rollback'].to_i
 
-          report_derived_metric "Database/Rows/Selected", 'rows', row['tup_returned'].to_i + row['tup_fetched'].to_i
-          report_derived_metric "Database/Rows/Inserted", 'rows', row['tup_inserted'].to_i
-          report_derived_metric "Database/Rows/Updated",  'rows', row['tup_updated'].to_i
-          report_derived_metric "Database/Rows/Deleted",  'rows', row['tup_deleted'].to_i
+          report_derived_metric 'Database/Rows/Selected', 'rows', row['tup_returned'].to_i + row['tup_fetched'].to_i
+          report_derived_metric 'Database/Rows/Inserted', 'rows', row['tup_inserted'].to_i
+          report_derived_metric 'Database/Rows/Updated',  'rows', row['tup_updated'].to_i
+          report_derived_metric 'Database/Rows/Deleted',  'rows', row['tup_deleted'].to_i
 
         end
       end
       @connection.exec(index_count_query) do |result|
-        report_metric "Database/Indexes/Count", 'indexes', result[0]['indexes'].to_i
+        report_metric 'Database/Indexes/Count', 'indexes', result[0]['indexes'].to_i
       end
       @connection.exec(index_size_query) do |result|
-        report_metric "Database/Indexes/Size", 'bytes', result[0]['size'].to_i
+        report_metric 'Database/Indexes/Size', 'bytes', result[0]['size'].to_i
       end
       @connection.exec(bgwriter_query) do |result|
-        report_derived_metric "Background Writer/Checkpoints/Scheduled", 'checkpoints', result[0]['checkpoints_timed'].to_i
-        report_derived_metric "Background Writer/Checkpoints/Requested", 'checkpoints', result[0]['checkpoints_requests'].to_i
+        report_derived_metric 'Background Writer/Checkpoints/Scheduled', 'checkpoints', result[0]['checkpoints_timed'].to_i
+        report_derived_metric 'Background Writer/Checkpoints/Requested', 'checkpoints', result[0]['checkpoints_requests'].to_i
       end
-      report_metric "Alerts/Index Miss Ratio", '%', calculate_miss_ratio(%Q{SELECT SUM(idx_blks_hit) AS hits, SUM(idx_blks_read) AS reads FROM pg_statio_user_indexes})
-      report_metric "Alerts/Cache Miss Ratio", '%', calculate_miss_ratio(%Q{SELECT SUM(heap_blks_hit) AS hits, SUM(heap_blks_read) AS reads FROM pg_statio_user_tables})
+      report_metric 'Alerts/Index Miss Ratio', '%', calculate_miss_ratio(%Q{SELECT SUM(idx_blks_hit) AS hits, SUM(idx_blks_read) AS reads FROM pg_statio_user_indexes})
+      report_metric 'Alerts/Cache Miss Ratio', '%', calculate_miss_ratio(%Q{SELECT SUM(heap_blks_hit) AS hits, SUM(heap_blks_read) AS reads FROM pg_statio_user_tables})
 
       # This is dependent on the pg_stat_statements being loaded, and assumes that pg_stat_statements.max has been set sufficiently high that most queries will be recorded. If your application typically generates more than 1000 distinct query plans per sampling interval, you're going to have a bad time.
-      if extension_loaded? "pg_stat_statements"
-        @connection.exec("SELECT SUM(calls) FROM pg_stat_statements") do |result|
-          report_derived_metric "Database/Statements", '', result[0]["sum"].to_i
+      if extension_loaded? 'pg_stat_statements'
+        @connection.exec('SELECT SUM(calls) FROM pg_stat_statements') do |result|
+          report_derived_metric 'Database/Statements', '', result[0]['sum'].to_i
         end
       else
-        puts "pg_stat_statements is not loaded; no Database/Statements metric will be reported."
+        puts 'pg_stat_statements is not loaded; no Database/Statements metric will be reported.'
       end
     end
 
@@ -109,7 +109,7 @@ module NewRelic::PostgresPlugin
 
       def extension_loaded?(extname)
         @connection.exec("SELECT count(*) FROM pg_extension WHERE extname = '#{extname}'") do |result|
-          result[0]["count"] == "1"
+          result[0]['count'] == '1'
         end
       end
 
@@ -128,8 +128,8 @@ module NewRelic::PostgresPlugin
         sample.each { |key,value| sample[key] = value.to_i }
         miss_ratio = if check_samples(@previous_result_for_query[query], sample)
 
-          hits = sample["hits"] - @previous_result_for_query[query]["hits"]
-          reads = sample["reads"] - @previous_result_for_query[query]["reads"]
+          hits = sample['hits'] - @previous_result_for_query[query]['hits']
+          reads = sample['reads'] - @previous_result_for_query[query]['reads']
           
           if (hits + reads) == 0
             0.0
@@ -178,7 +178,7 @@ module NewRelic::PostgresPlugin
       end
 
       def bgwriter_query
-        "SELECT * FROM pg_stat_bgwriter;"
+        'SELECT * FROM pg_stat_bgwriter;'
       end
 
       def index_count_query
@@ -186,7 +186,7 @@ module NewRelic::PostgresPlugin
       end
 
       def index_size_query
-        "SELECT sum(relpages::bigint*8192) AS size FROM pg_class WHERE reltype = 0;"
+        'SELECT sum(relpages::bigint*8192) AS size FROM pg_class WHERE reltype = 0;'
       end
 
   end
